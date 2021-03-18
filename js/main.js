04ecf7fcd4ed99429c19146d59d4ca7aa4a369f6
Giovanni <giovannipor23@gmail.com>
2021-03-18 19:52:01 +0100

    print Img

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: "#title",
     data: {
-        msg: "Hello World"
+        msg: "Hello World!",
+        image: "https://puzzledpagan.files.wordpress.com/2015/12/051.jpg"
     }
 });
